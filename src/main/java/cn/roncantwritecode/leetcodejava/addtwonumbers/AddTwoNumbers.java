fbd51c51fc85a13d7c449e1026072dbe82113ff2
wurunlong <wurunlong@sinosafe.com.cn>
2020-03-23 15:02:59 +0800

    两数相加提交

---
+++ src/main/java/cn/roncantwritecode/leetcodejava/addtwonumbers/AddTwoNumbers.java
@@ -0,0 +1,17 @@
+package cn.roncantwritecode.leetcodejava.addtwonumbers;
+
+/**
+ * @author wurunlong
+ * @date 2020/3/23
+ */
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        AddTwoNumbers twoSum = new AddTwoNumbers();
+
+    }
+
+    /*public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+    }*/
+}
---
+++ src/main/java/cn/roncantwritecode/leetcodejava/addtwonumbers/ListNode.java
@@ -0,0 +1,30 @@
+package cn.roncantwritecode.leetcodejava.addtwonumbers;
+
+/**
+ * @author wurunlong
+ * @date 2020/3/23
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
